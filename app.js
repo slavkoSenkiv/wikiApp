95764b9375e5e2eaeef94a85011c15c1b8a05036
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-05-25 08:34:45 +0300

    add mongoose boilserplate

--- app.js
+++ app.js
@@ -13,8 +13,19 @@ mongoose.connect(url, {
   useUnifiedTopology: true})
   .then(console.log('mongoose connection to db is successfull'));
 
+const articleSchema = new mongoose.Schema({
+    title: String,
+    content: String
+});
+
+const Article = mongoose.model('Article', articleSchema);
+
 /* const user = new User({ name: 'Slav1', age: 29 });
 user.save()
   .then(() => console.log('mongoose user saved'))
   .catch((error) => console.error(error))
   .finally(() => mongoose.disconnect()); */
+
+  app.listen(3000, ()=>{
+    console.log('server is up and listening to port 3000');
+  });
